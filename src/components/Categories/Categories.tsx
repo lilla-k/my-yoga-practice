534d345b59a58c06c0df9cee8eb9db4624c25717
lilla-k <kalmarlilianna@gmail.com>
2025-06-25 00:17:36 +0200

    #3 categories added with images

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,6 +1,27 @@
-function Categories(){
-    return(
-        <div>4 illustration</div>
+import './Categories.css';
+
+function Categories() {
+
+    const categories = [
+        { name: "Sitting", image: "/Copilot_sitting_illustration.png" },
+        { name: "Standing", image: "/Copilot_standing_illustration.png" },
+        { name: "Supine", image: "/Copilot_supine_illustration.png" },
+        { name: "Inversion", image: "/Copilot_inversion_illustration.png" }
+    ]
+    return (
+        <div className="Categories">
+            <h1 className="Categories-title">Categories</h1>
+            <div className="Categories-container">
+                {categories.map(c =>
+                    <div className="Categories-card">
+                        <img src={c.image} alt={c.name} className="Categories-image" />
+                        <div className="Categories-name">{c.name}</div>
+                    </div>
+                )}
+            </div>
+
+        </div>
+
     )
 }
 
