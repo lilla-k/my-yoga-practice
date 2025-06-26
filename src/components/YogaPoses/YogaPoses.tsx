5e34faf8035ad6df3932876cbbd2a8234ff95735
lilla-k <kalmarlilianna@gmail.com>
2025-06-26 08:52:40 +0200

    #6 YogaPoses component tabs mui

--- src/components/YogaPoses/YogaPoses.tsx
+++ src/components/YogaPoses/YogaPoses.tsx
@@ -1,14 +1,33 @@
-import { useParams } from 'react-router';
+import { useParams, useNavigate } from 'react-router';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import './YogaPoses.css'
 
-function YogaPoses(){
+function YogaPoses() {
 
-    const {category} = useParams();
-
-
-    return(
-        <div>
-            <div>Poses</div>
-            <div>{category}</div>
+    const { category } = useParams();
+    const navigate = useNavigate();
+    console.log(category);
+    const categories = [
+        { name: "sitting", image: "/Copilot_sitting_illustration.png" },
+        { name: "standing", image: "/Copilot_standing_illustration.png" },
+        { name: "supine", image: "/Copilot_supine_illustration.png" },
+        { name: "inversion", image: "/Copilot_inversion_illustration.png" }
+    ]
+    return (
+        <div className="YogaPoses">
+            <h1 className="YogaPoses-title">Poses</h1>
+            <Tabs
+                value={category}
+                onChange={(e:  React.SyntheticEvent, newValue: string) => navigate(`/yoga-poses/${newValue}`)}
+                textColor="secondary"
+                indicatorColor="secondary"
+                aria-label="secondary tabs example"
+            >
+                {categories.map(c =>
+                    <Tab value={c.name} label={c.name} />
+                )}
+            </Tabs>
         </div>
     )
 }
