316bfa79e6c465762199fc5523d102a3ecc2be9d
lilla-k <kalmarlilianna@gmail.com>
2026-02-03 09:42:46 +0100

    #20 header modified

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import { Link, useNavigate } from "react-router";
 import Tooltip from '@mui/material/Tooltip';
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import PersonIcon from '@mui/icons-material/Person';
+import HomeIcon from '@mui/icons-material/Home';
 import './Header.css';
 
 function Header() {
@@ -10,13 +10,16 @@ function Header() {
 
     return(
         <div className="Header">
-            <Link to="/" className="Header-title">
+            <Link to="/" className="Header-left">
                 <img className="Header-logo" src="/lotus_logo.png"/>
                 My Yoga Practice
             </Link>
-            <div className="Header-navbar">
-                <div onClick={()=> navigate("/yoga-asanas/all")}>Asanas</div>
-                <Tooltip title="Create" className="tooltip"><AddCircleOutlineIcon onClick={()=> navigate("/add-asana")}/></Tooltip>
+            <div className="Header-center">
+                <div className="Header-menu-item" onClick={()=> navigate("/")}><HomeIcon /></div>
+                <div className="Header-menu-item" onClick={()=> navigate("/yoga-asanas/all")}>Asanas</div>
+                <div className="Header-menu-item" onClick={()=> navigate("/builder")}>Sequence builder</div>
+            </div>
+            <div className="Header-right Header-menu-item">
                 <Tooltip title="Profile" className="tooltip"><PersonIcon/></Tooltip>
             </div>
         </div>
