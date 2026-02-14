78accd10c7ef7530804a04788297d46680926f19
lilla-k <kalmarlilianna@gmail.com>
2026-02-14 09:24:53 +0100

    #23 filter button bind to category

--- src/components/AsanasFilter/AsanasFilter.tsx
+++ src/components/AsanasFilter/AsanasFilter.tsx
@@ -1,31 +1,51 @@
-
+import { useState } from 'react';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
+import Button from '@mui/material/Button';
 import Select from '@mui/material/Select';
 import './AsanasFilter.css';
 
 
-function AsanasFilter({filters, handleChange}) {
+function AsanasFilter({filters, onFilterChange}) {
 
     console.log("filters", filters)
+    const [category, setCategory] = useState(filters.selectedCategory || "All categories");
+    console.log("selectedCategory", category);
+    const [level, setLevel] = useState(filters.selectedLevel || "All levels");
+    console.log("selectedLevel", level);
     const categories = ["All categories", "Sitting", "Standing", "Supine", "Inversion", "Prone"];
+    const levels = ["All levels", "Beginner", "Intermediate", "Advanced"];
 
     return (
-        <div>
+        <div className="AsanasFilter">
             <FormControl fullWidth>
                 <InputLabel id="demo-simple-select-label">Category</InputLabel>
                 <Select
                     labelId="demo-simple-select-label"
                     id="demo-simple-select"
                     defaultValue={"All categories"}
-                    value={filters.selectedCategory}
+                    value={category}
                     label="Age"
-                    onChange={handleChange}
+                    onChange={event => setCategory(event.target.value)}
                 >
                     {categories.map(c => <MenuItem value={c}>{c}</MenuItem>)}
                 </Select>
             </FormControl>
+            <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Level</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    defaultValue={"All categories"}
+                    value={filters.selectedLevel}
+                    label="Age"
+                    onChange={event => setLevel(event.target.value)}
+                >
+                    {levels.map(l => <MenuItem value={l}>{l}</MenuItem>)}
+                </Select>
+            </FormControl>
+            <Button variant="contained" onClick={() => onFilterChange(category, level)}>Filter</Button>
         </div>
     )
 }
--- src/components/AsanasPage/AsanasPage.tsx
+++ src/components/AsanasPage/AsanasPage.tsx
@@ -7,19 +7,24 @@ import './AsanasPage.css';
 
 function AsanasPage() {
 
-    const [searchParams, setSearchParams] = useSearchParams({category: "All categories"});
-    const selectedCategory = searchParams.get("category");
-    const filters = {selectedCategory: selectedCategory};
-    const handleChange = (event: SelectChangeEvent) => {
-        setSearchParams({category: event.target.value});
+    const [searchParams, setSearchParams] = useSearchParams({});
+    
+    const onFilterChange = (category, level) => {
+        setSearchParams({ category: category, level: level });
     };
 
+    const selectedCategory = searchParams.get("category");
+    const selectedLevel = searchParams.get("level");
+    const filters = {selectedCategory, selectedLevel};
 
     return (
         <div className="AsanasPage">
-            <h1 className="AsanasPage-title">Asanas library</h1>
-            <AsanasFilter filters={filters} handleChange = {handleChange}/>
-            <AsanasList filters={filters}/>
+            <div className="AsanasPage-content">
+                <h1 className="AsanasPage-title">Asanas library</h1>
+                <AsanasFilter filters={filters} onFilterChange={onFilterChange} />
+                <AsanasList filters={filters} />
+            </div>
+
         </div>
     )
 }
